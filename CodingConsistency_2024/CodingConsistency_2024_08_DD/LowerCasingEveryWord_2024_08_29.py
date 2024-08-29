befd62a78921ef186bdc0d8d63237f6ed20d6f79
Aljibra <azkaaljibra123@gmail.com>
2024-08-29 08:35:10 -0700

    LeetCodeProblemLowerCase

---
+++ CodingConsistency_2024/CodingConsistency_2024_08_DD/LowerCasingEveryWord_2024_08_29.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def toLowerCase(self, s):
+        x = s.lower()
+        return x
+
+
+
+# Mistakes of mine
+
+#class Solution(object):
+#    def toLowerCase(self, s):
+#        s.lower()
+#        return s
+#
+
+# Doesn't work because it'll return a new string instead of 
+# changing the original string
+
+# Should probably use docstrings next time..
