2af3f459642dfeb3b5c70350840afa3c5303fc6d
Aljibra <azkaaljibra123@gmail.com>
2024-08-30 15:16:51 -0700

    OOP Practices on strings

---
+++ CodingConsistency_2024/CodingConsistency_2024_08_DD/OOPPractices_2024_08_30.py
@@ -0,0 +1,12 @@
+class OOP():
+
+    def __init__(self,words,anotherword) -> None:
+        self.words = words
+        self.anotherword = anotherword
+
+    def stringfunction(self) -> str:
+        print("This is ", self.words, "And This is ", self.anotherword)
+
+
+obj = OOP("Alan","Timmy")
+obj.stringfunction()
