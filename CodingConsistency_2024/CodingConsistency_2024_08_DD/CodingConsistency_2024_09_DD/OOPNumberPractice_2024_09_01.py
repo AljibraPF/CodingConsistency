c6e0bd27b6df2a7fdb7cce8b78de1295dc76492d
Aljibra <azkaaljibra123@gmail.com>
2024-09-01 17:13:03 -0700

    LearningNumeroperatorOOP

---
+++ CodingConsistency_2024/CodingConsistency_2024_08_DD/CodingConsistency_2024_09_DD/OOPNumberPractice_2024_09_01.py
@@ -0,0 +1,24 @@
+class NumbersOnumbers:
+    def __init__(self,num1,num2) -> None:
+        self.num1 = num1
+        self.num2 = num2
+    
+    def additioningSum(self):
+        print(self.num1 + self.num2)
+    def subtractingSum(self):
+        print(self.num1 - self.num2)
+    def multiplyingSum(self):
+        print(self.num1 * self.num2)
+    def dividingSum(self):
+        print(self.num1 / self.num2)
+    
+obj = NumbersOnumbers(5,10)
+
+obj.additioningSum()
+obj.subtractingSum()
+obj.multiplyingSum()
+obj.dividingSum()
+
+# Code Seems too much of an overkill for an OOP 
+# I Don't think Every Code should require an OOP paradigm..
+# Perhaps i should continue with the functional and procedure paradigm?
