3515ae7585fa75ed2c5430ed66a6a66988c230dd
Aljibra <azkaaljibra123@gmail.com>
2024-09-02 09:13:27 -0700

    Added Dictionaries Print Format

--- CodingConsistency_2024/CodingConsistency_2024_09_DD/OOPNumberPractice_2024_09_01.py
+++ CodingConsistency_2024/CodingConsistency_2024_09_DD/OOPNumberPractice_2024_09_01.py
@@ -4,20 +4,34 @@ class NumbersOnumbers:
         self.num2 = num2
     
     def additioningSum(self):
-        print(self.num1 + self.num2)
-    def subtractingSum(self):
-        print(self.num1 - self.num2)
-    def multiplyingSum(self):
-        print(self.num1 * self.num2)
-    def dividingSum(self):
-        print(self.num1 / self.num2)
+        return self.num1 + self.num2
     
-obj = NumbersOnumbers(5,10)
+    def subtractingSum(self):
+        return self.num1 - self.num2
+    
+    def multiplyingSum(self):
+        return self.num1 * self.num2
+    
+    def dividingSum(self):
+        if self.num2 != 0:
+            return self.num1 / self.num2
+        else:
+            print("Can't Divide By 0")
 
-obj.additioningSum()
-obj.subtractingSum()
-obj.multiplyingSum()
-obj.dividingSum()
+    def calculate_all(self):
+        return {
+            "additioning" : self.additioningSum(),
+            "subtracting" : self.subtractingSum(),
+            "multiplying" : self.multiplyingSum(),
+            "dividing" : self.dividingSum() 
+        }
+
+x = int(input("Input 1: "))
+y = int(input("Input 2: "))
+
+calc = NumbersOnumbers(x,y)
+results = calc.calculate_all()
+print(results)
 
 # Code Seems too much of an overkill for an OOP 
 # I Don't think Every Code should require an OOP paradigm..
