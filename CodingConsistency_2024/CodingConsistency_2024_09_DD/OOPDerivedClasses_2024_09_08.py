bdb97c9fa65b0aabf91c197bd0dd61afad77c32b
Aljibra <azkaaljibra123@gmail.com>
2024-09-08 05:45:21 -0700

    Added a derived triangle class

--- CodingConsistency_2024/CodingConsistency_2024_09_DD/OOPDerivedClasses_2024_09_08.py
+++ CodingConsistency_2024/CodingConsistency_2024_09_DD/OOPDerivedClasses_2024_09_08.py
@@ -28,6 +28,20 @@ class Rectangle(PHShape):
     def calc_perimeter(self):
         return 2 * (self.length + self.width)
     
+class Triangle(PHShape):
+    def __init__(self,base,height,s1,s2,s3):
+        self.base = base
+        self.height = height
+        self.s1 = s1
+        self.s2 = s2
+        self. s3 = s3
+    
+    def calc_area(self):
+        return 0.5 * self.base * self.height
+    
+    def calc_perimeter(self):
+        return self.s1 + self.s2 + self.s3
+    
 
 
 
@@ -36,6 +50,14 @@ print(f"Result Of Circle Area: {circleshape.calc_area()}")
 print(f"Result of Circle Perimeter: {circleshape.calc_perimeter()}")
 
 squareshape = Rectangle(5,5)
-print(f"Result Of Circle Area: {squareshape.calc_area()}")
-print(f"Result Of Circle Area: {squareshape.calc_perimeter()}")
+print(f"\nResult Of Rectangle Area: {squareshape.calc_area()}")
+print(f"Result Of Rectangle Perimeter: {squareshape.calc_perimeter()}")
+
+triangshape = Triangle(5,6,7,2,5)
+print(f"\nResult Of Triangle Area: {triangshape.calc_area()}")
+print(f"Result Of Triangle Perimeter: {triangshape.calc_perimeter()}")
+
+#Practice to try and use inheritance classes
+
+
 
