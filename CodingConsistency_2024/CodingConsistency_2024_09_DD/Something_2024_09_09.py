2bbd9d75185a5b74efdde3cf8c8530054b3c1548
Aljibra <azkaaljibra123@gmail.com>
2024-09-09 16:25:23 -0700

    Method to calculate current age

--- CodingConsistency_2024/CodingConsistency_2024_09_DD/Something_2024_09_09.py
+++ CodingConsistency_2024/CodingConsistency_2024_09_DD/Something_2024_09_09.py
@@ -1,17 +1,5 @@
 from datetime import date
 
-today = date.today()
-
-x = today.year
-
-print(x)
-
-
-
-
-'''
-from datetime import date
-
 class Human:
     def __init__(self,name,country,doy): #doy is date of year
         self.name = name
@@ -19,8 +7,23 @@ class Human:
         self.doy = doy
 
     def currentAge(self):
-        return self.currentYear - self.doy
+        today = date.today()
+        current_year = today.year
+        return current_year - self.doy
 
     
-obj = Human("Alif","Indonesia", 2021)
+obj = Human("Alif","Indonesia", 2007)
+print(obj.currentAge())
+
+
+
+# Gets the current year
+'''
+from datetime import date
+
+today = date.today()
+
+x = today.year
+
+print(x)
 '''
