ee92a8b24b5fe243c29583b90945d3d46beaf669
Aljibra <azkaaljibra123@gmail.com>
2024-09-19 17:22:19 +0800

    Learning to run C

---
+++ CodingConsistency_2024/CodingConsistency_2024_09_DD/CRunningHeadache_2024_09_19.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  printf("Hello World!");
+  return 0;
+}
+
+/*
+
+Woah what a mess, had to like install so many applications just to run this C File
+Turns out you need to "Build" the file first then you can run it.
+I Need some feedback on this
+
+*/
