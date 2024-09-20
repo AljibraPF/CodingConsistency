442a4fb5303e4e95824b1313ef05446b8d6eca52
Aljibra <azkaaljibra123@gmail.com>
2024-09-20 13:34:04 +0800

    Learning to show output On C

---
+++ CodingConsistency_2024/CodingConsistency_2024_09_DD/TestingC_2024_09_20.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello There\n\n"); //Woah! it still uses \n to make a new line? awesome
+    printf("I'm Learning C Right now!\n\n");
+    int SingleNumber = 10; //Use format specifiers to show output? Wew
+    char SingleLetter = 'A';
+    printf("This is Number %d and This is Char %c \n",SingleNumber,SingleLetter);
+    return 0;
+}
+
+//%c for char
+//%d for int
+//%f for float
