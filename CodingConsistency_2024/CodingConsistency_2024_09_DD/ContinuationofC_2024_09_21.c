f5ec0aa741513d2c1a0de2a01905f61bdc78283f
Aljibra <azkaaljibra123@gmail.com>
2024-09-21 17:02:50 +0800

    Sum and assign

---
+++ CodingConsistency_2024/CodingConsistency_2024_09_DD/ContinuationofC_2024_09_21.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int myNum = 15;
+    int OtherNum;
+    OtherNum = myNum;
+    printf("%d\n", OtherNum);
+
+    int x = 20;
+    int y = 30;
+    int xy = x + y;
+    printf("%d\n", xy);
+}
+
