eda6cb436cf1b652015d4835aa06a8c879ac5ebc
Aljibra <azkaaljibra123@gmail.com>
2024-09-25 08:18:53 +0800

    Placed Input

--- CodingConsistency_2024/CodingConsistency_2024_09_DD/LeetCodePalindromeProblem_2024_09_25.py
+++ CodingConsistency_2024/CodingConsistency_2024_09_DD/LeetCodePalindromeProblem_2024_09_25.py
@@ -5,5 +5,10 @@ class Solution():
 
         stringx = str(x)
 
-        return stringx == stringx[::-1]
+        return stringx == stringx[::-1] # Formula for palindrome
 
+obj = Solution()
+y = int(input("Number: "))
+print(f"Palindrome? : {obj.isPalindrome(y)}")
+
+#How in the world did somone manage to run it in only 7 Ms? Maybe used different language.
