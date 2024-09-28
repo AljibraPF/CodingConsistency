63de09ab32d065a27d60472f3a8b35d6a5c1b870
Aljibra <azkaaljibra123@gmail.com>
2024-09-28 18:51:53 +0800

    Comparing C

---
+++ CodingConsistency_2024/BooleanComparisonPrint_2024_09_28.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdio.h>
+//Least comparisons not much different!
+int main(){
+    int num1 = 10;
+    int num2 = 20;
+    printf("1 is True\n0 is False\nResult: %d\n", num1 <= num2);
+}
---
+++ CodingConsistency_2024/CodingConsistency_2024_09_DD/BooleanOfC_2024_09_27.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdbool.h>
+// 0 is false and any other number than 0 is True
+// Doesn't have built in boolean values? Must import first.
+int main(){
+    bool BooleanValue = true;
+    printf("%d",BooleanValue);
+    return 0;
+}
