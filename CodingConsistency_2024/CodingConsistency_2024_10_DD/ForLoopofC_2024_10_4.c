badb7a7544eaae97a281a2c6731ec472e3e08766
Aljibra <azkaaljibra123@gmail.com>
2024-10-04 21:03:21 +0800

    ForLooping

---
+++ CodingConsistency_2024/CodingConsistency_2024_10_DD/ForLoopofC_2024_10_4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+//Varians of a bunch of
+
+int main() {
+    int i;
+
+    printf("Increase loop:\n");
+    for (i = 0; i < 5; i++) {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
