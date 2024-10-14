b1b412dcf87a08e060d5861ed7c76df0191602cc
Aljibra <azkaaljibra123@gmail.com>
2024-10-14 16:00:52 +0800

    Temperature Example of numpy

--- CodingConsistency_2024/CodingConsistency_2024_10_DD/GainingFooting_2024_10_14.py
+++ CodingConsistency_2024/CodingConsistency_2024_10_DD/GainingFooting_2024_10_14.py
@@ -4,13 +4,26 @@ import numpy as np
 arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
 mean_value = np.mean(arr)
-print("Mean of the array:", mean_value)
+print("Mean:", mean_value)
 
 sum_value = np.sum(arr)
-print("Sum of the array:", sum_value)
+print("Sum:", sum_value)
 
 max_value = np.max(arr)
-print("Maximum value in the array:", max_value)
+print("Maximum:", max_value)
 
 reshaped_arr = arr.reshape(2, 5)
 print("Reshaped array:\n", reshaped_arr)
+
+#Real life example of it being used
+
+temperatures = np.array([22, 25, 23, 20, 24, 26, 21])
+
+average_temp = np.mean(temperatures)
+print(f"Average temp: {average_temp:.2f}°C")
+
+hottest_temp = np.max(temperatures)
+print(f"Hottest temp: {hottest_temp}°C")
+
+coldest_temp = np.min(temperatures)
+print(f"Coldest temp: {coldest_temp}°C")
