ec3ecbe8f900158c6541a9eeff92dfb2e1ac76fb
Aljibra <azkaaljibra123@gmail.com>
2024-11-18 20:05:00 +0800

    Memory Management

---
+++ CodingConsistency_2024/CodingConsistency_2024_11_DD/MemoryManagement_2024_11_18.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+//Managing Memory
+int main() {
+    int Integers;
+    float Floats;
+    double Doubled;
+    char Chars;
+
+    printf("%lu\n", sizeof(Integers));     
+    printf("%lu\n", sizeof(Floats));    
+    printf("%lu\n", sizeof(Doubled));   
+    printf("%lu\n", sizeof(Chars));     
+}
