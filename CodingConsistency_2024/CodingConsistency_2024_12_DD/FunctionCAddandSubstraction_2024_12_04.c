43002a6cc0cf09e3427a1aab73d819953abbf1ad
Aljibra <azkaaljibra123@gmail.com>
2024-12-04 09:44:33 +0800

    Adding And Substraction with Functions C

---
+++ CodingConsistency_2024/CodingConsistency_2024_12_DD/FunctionCAddandSubstraction_2024_12_04.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int calculate(int a, int b, char operation) {
+    if (operation == '+') {
+        return a + b;
+    } else if (operation == '-') {
+        return a - b;
+    } else {
+        printf("Invalid Operation(Use ' + ' Or ' - ') \n");
+        return 0;
+    }
+
+}
+
+int main() {
+    int num1 = 10, num2 = 5;
+    char op = '+';
+
+    int result = calculate(num1,num2,op);
+    printf("Result: %d\n", result);
+
+    return 0;
+}
