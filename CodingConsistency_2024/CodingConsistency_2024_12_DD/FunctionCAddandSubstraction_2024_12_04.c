f2bb33b64a18265178bc282f611c31aa0775fdf3
Aljibra <azkaaljibra123@gmail.com>
2024-12-05 08:36:06 +0800

    Added Multiplication and Division

--- CodingConsistency_2024/CodingConsistency_2024_12_DD/FunctionCAddandSubstraction_2024_12_04.c
+++ CodingConsistency_2024/CodingConsistency_2024_12_DD/FunctionCAddandSubstraction_2024_12_04.c
@@ -1,23 +1,31 @@
 #include <stdio.h>
 //Adding and substractions in C
+//12/5/2024
+//Added Multiplication and Division
 int calculate(int a, int b, char operation) {
     if (operation == '+') {
         return a + b;
     } else if (operation == '-') {
         return a - b;
+    } else if (operation == '*') {
+        return a * b;
+    } else if (operation == '/') {
+        return a / b;
     } else {
-        printf("Invalid Operation(Use ' + ' Or ' - ') \n");
+        return printf("Not a Valid Operator");
         return 0;
     }
-
 }
 
 int main() {
     int num1 = 10, num2 = 5;
-    char op = '-';
+    char op = '/';
 
     int result = calculate(num1,num2,op);
     printf("Result: %d\n", result);
 
     return 0;
 }
+
+
+
