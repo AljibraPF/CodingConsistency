e6cef86bb2cd4c64778420eec34bbae1b9565b52
Aljibra <azkaaljibra123@gmail.com>
2024-12-06 09:44:14 +0800

    Added Input

--- CodingConsistency_2024/CodingConsistency_2024_12_DD/FunctionCAddandSubstraction_2024_12_04.c
+++ CodingConsistency_2024/CodingConsistency_2024_12_DD/FunctionCAddandSubstraction_2024_12_04.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 //Adding and substractions in C
+
 //12/5/2024
 //Added Multiplication and Division
+
+//12/6/2024
+//Added Input for flexibility
+
 int calculate(int a, int b, char operation) {
     if (operation == '+') {
         return a + b;
@@ -18,11 +23,21 @@ int calculate(int a, int b, char operation) {
 }
 
 int main() {
-    int num1 = 10, num2 = 5;
-    char op = '/';
+
+    int num1,num2;
+    char op;
+
+    printf("\nNum 1: ");
+    scanf("%d", &num1);
+
+    printf("\nNum 2: ");
+    scanf("%d", &num2);
+
+    printf("\nOperator: ");
+    scanf(" %c", &op);
 
     int result = calculate(num1,num2,op);
-    printf("Result: %d\n", result);
+    printf("\nResult: %d\n", result);
 
     return 0;
 }
